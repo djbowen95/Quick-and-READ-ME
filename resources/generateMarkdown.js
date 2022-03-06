33cb2b4f0950b745b37d4bed0bcd238d4609dfa4
Daniel Bowen <djbowen95@gmail.com>
2022-03-06 22:50:48 +0000

    Store template, fill gaps with user input

--- resources/generateMarkdown.js
+++ resources/generateMarkdown.js
@@ -5,7 +5,7 @@ function renderLicenseBadge(license) {
     } else {
       return "" // Return an empty string if input is 'none'.
     }
-  }
+}
 
 // Render a license link based on input.
 function renderLicenseLink(license) {
@@ -14,4 +14,55 @@ function renderLicenseLink(license) {
     } else {
       return "" // Return an empty string if input is 'none'.
     }
+}
+
+// Render license section based on input.
+function renderLicenseSection(license) {
+    if (license !== "None") {
+      return `## License
+      
+      This project is licensed under ${license}.`
+    } else {
+      return "" // Return an empty string if input is 'none'.
+    }
+}
+
+// Template for the markdown file - fills in gaps with 'data' from user input.
+function generateMarkdown(data) {
+    return `# ${data.title}
+    ${renderLicenseBadge(data.license)}
+    
+    ## Description
+    ${data.description}
+  
+    ## Table of Contents
+    
+    * [Installation](#installation)
+    * [Usage](#usage)
+    ${renderLicenseLink(data.license)}
+    * [Contribution](#contribution)
+    * [Tests](#tests)
+    * [Questions](#questions)
+  
+    ## Installation
+    ${data.installation}
+  
+    ## Usage
+    ${data.usage}
+  
+    ${renderLicenseSection(data.license)}
+  
+    ## Contribution
+    ${data.contribution}
+  
+    ## Tests
+    ${data.test}
+  
+    ## Questions
+    If you have any questions about this project, please contact me at:
+    Email: ${data.email}
+    GitHub: [${data.github}](https://github.com/${data.github})
+  `;
   }
+  
+  module.exports = generateMarkdown;
