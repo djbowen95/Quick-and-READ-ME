b208b0795658ec53ff44b36eae02708a0bd0266d
Daniel Bowen <djbowen95@gmail.com>
2022-03-06 22:47:49 +0000

    Render license link based on input

--- resources/generateMarkdown.js
+++ resources/generateMarkdown.js
@@ -1,8 +1,17 @@
-// Render a license badge based on a license input. 
+// Render a license badge based on a license input.
 function renderLicenseBadge(license) {
     if (license !== "None") {
       return `<img src="https://img.shields.io/badge/license-${license}-green.svg">`
     } else {
-      return ""
+      return "" // Return an empty string if input is 'none'.
+    }
+  }
+
+// Render a license link based on input.
+function renderLicenseLink(license) {
+    if (license !== "None") {
+      return `* [License](#license)`
+    } else {
+      return "" // Return an empty string if input is 'none'.
     }
   }
