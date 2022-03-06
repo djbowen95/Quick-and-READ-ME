ef7b841ac7c8e84b290a2e987acd719de2639279
Daniel Bowen <djbowen95@gmail.com>
2022-03-06 22:35:18 +0000

    Write a file using fs and path modules

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const questions = require("./resources/questions");
+// Import packages required for the program to run.
+const inquirer = require("inquirer"); // Runs the prompts in the terminal.
+const fs = require("fs"); // Will be used to write README to the file system.
+const path = require("path"); // Will be used to create a new path to store the README file.
+const questions = require("./resources/questions"); // Contains questions.
+const generateMarkdown = require("./resources/generateMarkdown.js") // Program that writes markdown file based on user responses. 
 
 // Introduce the application in the console.
 console.log("\n Hello, Welcome to the Easy Read Me Generator!\n");
 console.log("The application will ask you a series of prompts.");
 console.log("It will generate a markdown README file based on your answers.\n");
 
-// Run the code to ask the questions, then return the answers as an object in the console.
-inquirer.prompt(questions).then((answers) => {
-  const ansObj = JSON.stringify(answers);
-  console.log(ansObj)
-});
+// Write a new file to the file system, containing data passed through as a parameter.
+function writeToFile(fileName, data) {
+  fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// Initiate the program.
+begin();
